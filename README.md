d6383408c8cd41cfb37500b5ae8c10cf82614786
sun.bo <st_sunbo@hotmail.com>
2018-02-07 11:57:16 +0800

    Get started. To config JPA datasource later.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SpringBootPlay
 Retrofit SBShop backend with SpringBoot 1.5.10
+
+I got started from
+http://start.spring.io/
---
+++ src/main/java/com/madoka/sunb0002/springbootdemo/SpringbootdemoApplication.java
@@ -0,0 +1,14 @@
+package com.madoka.sunb0002.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class SpringbootdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdemoApplication.class, args);
+	}
+}
