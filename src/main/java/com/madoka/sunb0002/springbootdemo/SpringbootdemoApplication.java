4b9083a15355ab393e634307c84ec511e94c49d7
sun.bo <st_sunbo@hotmail.com>
2018-02-08 02:08:13 +0800

    missing file

---
+++ src/main/java/com/madoka/sunb0002/springbootdemo/SpringbootdemoApplication.java
@@ -0,0 +1,48 @@
+package com.madoka.sunb0002.springbootdemo;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+/**
+ * @see @SpringBootApplication = @Configuration + @EnableAutoConfiguration
+ *      + @ComponentScan
+ * @see SpringBootServletInitializer = (Old Spring) WebApplicationInitializer
+ * @author Sun Bo
+ *
+ */
+@SpringBootApplication
+public class SpringbootdemoApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdemoApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootdemoApplication.class);
+	}
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+
+		AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+		rootContext.register(RootConfig.class);
+		servletContext.addListener(new ContextLoaderListener(rootContext));
+		servletContext.setInitParameter("defaultHtmlEscape", "true");
+
+		ServletRegistration.Dynamic registration = servletContext.addServlet("dispatcher",
+				new DispatcherServlet(rootContext));
+		registration.setLoadOnStartup(1);
+		registration.addMapping("/*");
+	}
+
+}
