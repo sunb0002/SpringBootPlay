458f40a0b0376b0facf5b7692cbf10bfaf5efc86
sun.bo <st_sunbo@hotmail.com>
2018-02-17 00:21:27 +0800

    MessageSource bean has been created externally.

--- src/main/java/com/madoka/sunb0002/springbootdemo/RootConfig.java
+++ src/main/java/com/madoka/sunb0002/springbootdemo/RootConfig.java
@@ -3,13 +3,9 @@
  */
 package com.madoka.sunb0002.springbootdemo;
 
-import java.nio.charset.StandardCharsets;
-
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.core.convert.support.DefaultConversionService;
 
@@ -21,8 +17,6 @@ import org.springframework.core.convert.support.DefaultConversionService;
 @ComponentScan("com.madoka.sunb0002.springbootdemo.config")
 public class RootConfig {
 
-	private static final String ENCODING_UTF_8 = StandardCharsets.UTF_8.name();
-
 	/**
 	 * Activates the new configuration service which supports converting String
 	 * to Collection types.
@@ -33,19 +27,6 @@ public class RootConfig {
 	@Bean
 	public ConversionService conversionService() {
 		return new DefaultConversionService();
-	}
-
-	/**
-	 * Defines message dictionary.
-	 * 
-	 * @return MessageSource
-	 */
-	@Bean("messageSource")
-	public MessageSource messageDictionary() {
-		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-		messageSource.setBasename("classpath:messages");
-		messageSource.setDefaultEncoding(ENCODING_UTF_8);
-		return messageSource;
 	}
 
 }
