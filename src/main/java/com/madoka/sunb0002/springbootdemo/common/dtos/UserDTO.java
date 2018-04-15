eca8065f074c6beed2d4aa7cca52c7b264a22301
sun.bo.mac <st_sunbo@hotmail.com>
2018-04-16 00:13:37 +0800

    Minor cleanup pom and lombok.

--- src/main/java/com/madoka/sunb0002/springbootdemo/common/dtos/UserDTO.java
+++ src/main/java/com/madoka/sunb0002/springbootdemo/common/dtos/UserDTO.java
@@ -5,10 +5,17 @@ package com.madoka.sunb0002.springbootdemo.common.dtos;
 
 import java.io.Serializable;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author Sun Bo
  *
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDTO implements Serializable {
 
 	/**
@@ -19,45 +26,5 @@ public class UserDTO implements Serializable {
 	private Long userId;
 	private String nric;
 	private String name;
-
-	public UserDTO() {
-		super();
-	}
-
-	public UserDTO(Long userId, String nric, String name) {
-		super();
-		this.userId = userId;
-		this.nric = nric;
-		this.name = name;
-	}
-
-	public Long getUserId() {
-		return userId;
-	}
-
-	public void setUserId(Long userId) {
-		this.userId = userId;
-	}
-
-	public String getNric() {
-		return nric;
-	}
-
-	public void setNric(String nric) {
-		this.nric = nric;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	@Override
-	public String toString() {
-		return "UserDTO [userId=" + userId + ", nric=" + nric + ", name=" + name + "]";
-	}
 
 }
